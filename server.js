c7b31604de24c6f3098b4850070e8334c9677f0d
Jakob Sorensen <boghsteren@gmail.com>
2018-04-27 19:50:13 +0200

    Added server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const next = require('next')
+
+const port = process.env.PORT || 3000
+
+const app = next({ port })
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, err => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
